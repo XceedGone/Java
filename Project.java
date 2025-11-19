050d5fb62cf859c0c35e8407588d6659305c9175
John Christopher Pesquera <jcpesquera.itso@tip.edu.ph>
2025-11-19 23:33:02 +0800

    Added options for user desired transactions

--- Project.java
+++ Project.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class Project {
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isLoggedIn = false;
 
         Credentials user_1 = new Credentials("test", "1234");
 
+        // User validation
         while(!isLoggedIn) {
             System.out.print("Enter username: ");
             String uName = scanner.nextLine();
@@ -20,6 +23,19 @@ public class Project {
                 System.out.println("Error! Please try again.");
             }
         }
+
+        // Contents
+        // To-do: Handle exceptionsn when use passes sting in choices. 
+        int choice;
+        do {
+            System.out.println(ANSI_GREEN  + "Please choose an option: "+ ANSI_RESET);
+            System.out.println("1. Product and Inventory database. ");
+            System.out.println("2. Employees and Performance database. ");
+            System.out.println("3. Logout and Exit ");
+            choice = Integer.parseInt(scanner.nextLine());
+            
+            System.out.println("You Choose : " + choice);
+        } while (choice != 3);
     }
 }
 
